9fffd279962a92833e658be503adfcbe04fbc51a
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-12-06 19:18:39 -0500

    Add initial data to db/seeds.rb and seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+ProgrammingLanguage.create([
+  { title: "Ruby", description: "A dynamic, open source programming language with a focus on simplicity and productivity.", link: "https://www.ruby-lang.org/" },
+  { title: "Python", description: "A powerful programming language known for its easy-to-read syntax and wide range of applications.", link: "https://www.python.org/" },
+  { title: "JavaScript", description: "A versatile language commonly used for web development to create interactive websites.", link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
+  { title: "Java", description: "A widely-used language designed for portability across platforms, commonly used in enterprise environments.", link: "https://www.java.com/" },
+  { title: "C#", description: "A modern, object-oriented programming language developed by Microsoft as part of its .NET initiative.", link: "https://learn.microsoft.com/en-us/dotnet/csharp/" },
+  { title: "C++", description: "An extension of C that includes object-oriented features, used for systems and application programming.", link: "https://www.cplusplus.com/" }
+])
